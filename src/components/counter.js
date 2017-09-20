809dfea1f3a855e0adc9d0b4addd6d20f37ce0b3
John Kagga <johnkagga@gmail.com>
2017-09-20 13:18:02 +0300

    Removed state from counter components and turned it into a stateless component

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,39 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class Counter extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            score: props.score
-        }
-    }
-
-    incrementScore() {
-        const score = (this.state.score + 1);
-        this.setState({
-            score: score
-        })
-    };
-
-    decrementScore() {
-        const score = (this.state.score - 1);
-        this.setState({
-            score: score
-        })
-    }
-
-    render() {
-        return <div className="player-score">
-            <div className="counter">
-                <button className="counter-action decrement" onClick={() => this.decrementScore()}>-</button>
-                <div className="counter-score">{this.state.score}</div>
-                <button className="counter-action increment" onClick={() => this.incrementScore()}>+</button>
-            </div>
+const Counter = props => (
+    <div className="player-score">
+        <div className="counter">
+            <button className="counter-action decrement">-</button>
+            <div className="counter-score">{props.score}</div>
+            <button className="counter-action increment">+</button>
         </div>
-    }
-}
+    </div>
+);
 
 Counter.propTypes = {
     score: PropTypes.number.isRequired
