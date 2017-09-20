e3459950f2641838577346cec8af5c3faf832f0f
John Kagga <johnkagga@gmail.com>
2017-09-20 12:58:10 +0300

    Incrementing and decrementing score values

--- src/components/counter.js
+++ src/components/counter.js
@@ -9,12 +9,26 @@ class Counter extends React.Component {
         }
     }
 
+    incrementScore() {
+        const score = (this.state.score + 1);
+        this.setState({
+            score: score
+        })
+    };
+
+    decrementScore() {
+        const score = (this.state.score - 1);
+        this.setState({
+            score: score
+        })
+    }
+
     render() {
         return <div className="player-score">
             <div className="counter">
-                <button className="counter-action decrement"> -</button>
+                <button className="counter-action decrement" onClick={() => this.decrementScore()}> -</button>
                 <div className="counter-score">{this.state.score}</div>
-                <button className="counter-action increment"> +</button>
+                <button className="counter-action increment" onClick={() => this.incrementScore()}> +</button>
             </div>
         </div>
     }
