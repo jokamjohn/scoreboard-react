7f711b03e7bcbc369ef1016598c427e09893ae42
John Kagga <johnkagga@gmail.com>
2017-09-20 13:28:04 +0300

    Made the Application component stateful

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,16 +4,29 @@ import '../css/styles.css';
 import Header from "./header";
 import Player from "./player";
 
-const Application = props => (
-    <div className="scoreboard">
-        <Header title={props.title}/>
-        {props.players.map(player => <Player key={player.id} {...player}/>)}
-    </div>
-);
+class Application extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            players: this.props.initialPlayers
+        }
+    }
+
+    render() {
+        return (
+            <div className="scoreboard">
+                <Header title={this.props.title}/>
+                {this.state.players.map(player => <Player key={player.id} {...player}/>)}
+            </div>
+        )
+    }
+}
+
 
 Application.propTypes = {
     title: PropTypes.string,
-    players: PropTypes.arrayOf(PropTypes.shape({
+    initialPlayers: PropTypes.arrayOf(PropTypes.shape({
         id: PropTypes.number.isRequired,
         name: PropTypes.string.isRequired,
         score: PropTypes.number.isRequired
--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import Application from './components/layout';
 import PLAYERS from "./components/data";
 
-ReactDOM.render(<Application players={PLAYERS}/>, document.getElementById('root'));
+ReactDOM.render(<Application initialPlayers={PLAYERS}/>, document.getElementById('root'));
