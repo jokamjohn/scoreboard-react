770636b2787c6d69ba3c81a579241272bf82a88c
John Kagga <johnkagga@gmail.com>
2017-09-21 21:31:30 +0300

    Added player reducer

---
+++ src/reducers/player.js
@@ -0,0 +1,55 @@
+import * as PlayerActionTypes from '../actiontypes/player';
+
+const initialState = [
+    {
+        id: 1,
+        name: "John Kagga",
+        score: 40
+    },
+    {
+        id: 2,
+        name: "Cecilia Caroline",
+        score: 20
+    },
+    {
+        id: 3,
+        name: "Mark Musa",
+        score: 25
+    },
+];
+
+export default function (state = initialState, action) {
+
+    switch (action.type) {
+        case PlayerActionTypes.ADD_PLAYER:
+            return [
+                ...state,
+                {
+                    id: action.id,
+                    name: action.name,
+                    score: 0
+                }
+            ];
+
+        case PlayerActionTypes.REMOVE_PLAYER:
+            return [
+                ...state.slice(0, action.index),
+                ...state.slice(action.index + 1)
+            ];
+
+        case PlayerActionTypes.UPDATE_PLAYER_SCORE:
+            return state.map((player, index) => {
+                if (index === action.index) {
+                    return {
+                        ...player,
+                        score: player.score + action.score
+                    }
+                }
+                return player
+            });
+
+        default:
+            return state;
+    }
+
+}
