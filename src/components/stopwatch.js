c42487f6c9efae1f3406cea79c29e7312594b2b7
John Kagga <johnkagga@gmail.com>
2017-09-21 19:13:22 +0300

    Updated stopwatch component

--- src/components/stopwatch.js
+++ src/components/stopwatch.js
@@ -2,14 +2,13 @@ import React from 'react';
 
 class StopWatch extends React.Component {
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             running: false,
             elapsedTime: 0,
             previousTime: 0,
         };
-        this.onTick = this.onTick.bind(this);
     }
 
     /**
@@ -22,22 +21,6 @@ class StopWatch extends React.Component {
     }
 
     /**
-     * This method is run every 100 milliseconds which is about 10 times a second.
-     * If the clock is running, we update the elapsed time and also set the previous
-     * time to now.
-     */
-    onTick() {
-        if (this.state.running) {
-            let now = Date.now();
-            let elapsedTime = this.state.elapsedTime + (now - this.state.previousTime);
-            this.setState({
-                elapsedTime: elapsedTime,
-                previousTime: Date.now()
-            })
-        }
-    }
-
-    /**
      * This method is called when the component is unmounted.
      * All the component cleanup is done here.
      * This enables us to clear the interval.
@@ -46,37 +29,54 @@ class StopWatch extends React.Component {
         clearInterval(this.interval)
     }
 
+
+    /**
+     * This method is run every 100 milliseconds which is about 10 times a second.
+     * If the clock is running, we update the elapsed time and also set the previous
+     * time to now.
+     */
+    onTick = () => {
+        if (this.state.running) {
+            let now = Date.now();
+            let elapsedTime = this.state.elapsedTime + (now - this.state.previousTime);
+            this.setState({
+                elapsedTime: elapsedTime,
+                previousTime: Date.now()
+            })
+        }
+    };
+
     /**
      * Here we start counting our milliseconds since 1970 epoch
      * and also indicate that the clock is ticking.
      */
-    onStart() {
+    onStart = () => {
         this.setState({
             running: true,
             previousTime: Date.now()
         })
-    }
+    };
 
     /**
      * Here we pause the ticking of the clock(counting of milliseconds)
      */
-    onStop() {
+    onStop = () => {
         this.setState({
             running: false
         })
 
-    }
+    };
 
     /**
      * Here we reset the elapsed time to zero and also start counting the
      * milliseconds again from now.
      */
-    onReset() {
+    onReset = () => {
         this.setState({
             elapsedTime: 0,
             previousTime: Date.now()
         })
-    }
+    };
 
     render() {
         const seconds = Math.floor(this.state.elapsedTime / 1000);
@@ -84,9 +84,9 @@ class StopWatch extends React.Component {
             <div className="stopwatch">
                 <h2>Stopwatch</h2>
                 <div className="stopwatch-time">{seconds}</div>
-                {this.state.running ? <button onClick={() => this.onStop()}>Stop</button> :
-                    <button onClick={() => this.onStart()}>Start</button>}
-                <button onClick={() => this.onReset()}>Reset</button>
+                {this.state.running ? <button onClick={this.onStop}>Stop</button> :
+                    <button onClick={this.onStart}>Start</button>}
+                <button onClick={this.onReset}>Reset</button>
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Scoreboard from './components/layout';
+import Scoreboard from './containers/scoreboard';
 import PLAYERS from "./components/data";
 
 ReactDOM.render(<Scoreboard initialPlayers={PLAYERS}/>, document.getElementById('root'));
